6d196ab4f26383f5eed0db3db8c6c828e820f75d
Vivian-Oliveira <costa.vivs@gmail.com>
2021-07-07 18:41:07 +0100

    criei componentes de cores, base url e theme

--- cookenu/src/App.js
+++ cookenu/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import theme from './constants/theme';
 import Router from './routes/Router'
+import { ThemeProvider } from '@material-ui/styles';
 
 const App = () => {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Router />
-    </div>
+    </ThemeProvider>
   )
 }
 
