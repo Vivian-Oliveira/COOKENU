ff6b5d6a8dbb4c8ebe6eaa2bdf1ec6b27d3afd41
Vivian-Oliveira <costa.vivs@gmail.com>
2021-07-07 23:28:25 +0100

    Fiz requisicao da tela de receitas, renderizacao das listas e botao, que ao clicar, ele direciona para a tela de adicionar receitas

---
+++ cookenu/src/components/RecipeCard/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+
+export const RecipeCardContainer = styled(Card)`
+  width: 250px;
+  margin: 10px
+`
+
+export const RecipeCardContent = styled(CardContent)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+`
--- cookenu/src/pages/RecipesListPage/RecipesListPage.js
+++ cookenu/src/pages/RecipesListPage/RecipesListPage.js
@@ -1,11 +1,42 @@
 import React from 'react'
-import  useProtectedPage  from '../../hooks/useProtectedPage'
+import useProtectedPage from '../../hooks/useProtectedPage'
+import RecipeCard from '../../components/RecipeCard/RecipeCard'
+import useRequestData from '../../hooks/useRequestData'
+import { BASE_URL } from '../../constants/urls'
+import { RecipeListContainer, AddRecipeButton } from './styled'
+import{ Add } from '@material-ui/icons'
+import { goToAddRecipes } from '../../routes/coordinator'
+import { useHistory } from 'react-router'
+
 const RecipesListPage = () => {
     useProtectedPage()
+    const history = useHistory()
+    const recipes = useRequestData([], `${BASE_URL}/recipe/feed`)
+
+    const onClickCard = () => {
+
+    }
+
+    const recipesCards = recipes.map((recipe) => {
+        return (
+            <RecipeCard
+                key={recipe.recipe_id}
+                title={recipe.title}
+                image={recipe.image}
+                onClick={onClickCard}
+            />
+        )
+    })
     return (
-        <div>
-            <h1>RecipesListPage</h1>
-        </div>
+        <RecipeListContainer>
+            {recipesCards}
+            <AddRecipeButton
+                color={"primary"}
+                onClick={() => goToAddRecipes(history)}
+            >
+                <Add />
+            </AddRecipeButton>
+        </RecipeListContainer>
     )
 }
 
